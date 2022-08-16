1dc528e7f0d23beaf589dfa20b8a61b2b67445a4
Fidel <59423@bears.smcc.edu>
2022-08-15 22:41:49 -0400

    added some comments

--- src/constants/constants.h
+++ src/constants/constants.h
@@ -5,7 +5,7 @@
 #ifndef NEW_CAMERA_HACK_CONSTANTS_H
 #define NEW_CAMERA_HACK_CONSTANTS_H
 
-#define NOP 0x90
-#define DEFAULT_TEST  0x89, 0x54, 0x24, 0x10
+#define NOP 0x90 //will be used a lot
+#define DEFAULT_TEST  0x89, 0x54, 0x24, 0x10 // just a random function to test
 
 #endif //NEW_CAMERA_HACK_CONSTANTS_H

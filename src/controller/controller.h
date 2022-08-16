33d4f19e94a591485e5a641cc9f058a1b60b3e78
Fidel <59423@bears.smcc.edu>
2022-08-15 20:25:08 -0400

    choices

--- src/controller/controller.h
+++ src/controller/controller.h
@@ -1,14 +1,16 @@
 //
 // Created by fidel on 8/15/2022.
 //
-
 #ifndef NEW_CAMERA_HACK_CONTROLLER_H
 #define NEW_CAMERA_HACK_CONTROLLER_H
+
+#include "../hack_file/camera_hack.h"
 
 struct Controller
 {
     int choice;
     void camera_options();
+    void choices();
     void controller_loop();
     void menu();
 };

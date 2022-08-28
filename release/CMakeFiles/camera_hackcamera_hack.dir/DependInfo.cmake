11a0d5d8df63328b44447c0dbc619521a9d342f0
Fidel <59423@bears.smcc.edu>
2022-08-28 17:40:04 -0400

    added release folder

---
+++ release/CMakeFiles/camera_hackcamera_hack.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/fidel/Desktop/github_projects/camera_angles_mod/src/main.cpp" "CMakeFiles/camera_hackcamera_hack.dir/src/main.cpp.obj" "gcc" "CMakeFiles/camera_hackcamera_hack.dir/src/main.cpp.obj.d"
+  "C:/Users/fidel/Desktop/github_projects/camera_angles_mod/src/mod_file/cam_mod/camera_hack.cpp" "CMakeFiles/camera_hackcamera_hack.dir/src/mod_file/cam_mod/camera_hack.cpp.obj" "gcc" "CMakeFiles/camera_hackcamera_hack.dir/src/mod_file/cam_mod/camera_hack.cpp.obj.d"
+  "C:/Users/fidel/Desktop/github_projects/camera_angles_mod/src/mod_file/controller/controller.cpp" "CMakeFiles/camera_hackcamera_hack.dir/src/mod_file/controller/controller.cpp.obj" "gcc" "CMakeFiles/camera_hackcamera_hack.dir/src/mod_file/controller/controller.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
